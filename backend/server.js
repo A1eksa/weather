0f61340f2bc236ae14d7b95c3ca6295a61f6087d
Aleksandra Safranko <aleksandra.safranko1@gmail.com>
2022-06-27 10:46:02 +0200

    filtered data

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
+// const fetch = require('node-fetch');
+import Fetch from 'react-fetch';
+import { filterData } from './utils.js';
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -12,11 +15,32 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get('/', async (request, response) => {
+  const api_url =
+    'https://api.openweathermap.org/data/2.5/weather?lat=59.3293&lon=18.0686&appid=39cc3cd47c5ea2b190a2f97c31df95bb&units=metric';
+  // single day call
+
+  // const api_url =
+  //   'https://api.openweathermap.org/data/2.5/forecast?lat=59.3293&lon=18.0686&appid=39cc3cd47c5ea2b190a2f97c31df95bb';
+  // forecast 5 day/ 3 hour
+
+  try {
+    const fetch_response = await fetch(api_url);
+    const json = await fetch_response.json();
+    const forecast = filterData(json);
+    response.json(forecast);
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+// https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=59.3293&lon=18.0686&appid=eecbdbcaf5898765ececc528ec1f76a8
+// 3hour forcast 5 day works 'https://api.openweathermap.org/data/2.5/forecast?lat=59.3293&lon=18.0686&appid=39cc3cd47c5ea2b190a2f97c31df95bb';
+
+// https://api.openweathermap.org/data/2.5/forecast/daily?lat=59.3293&lon=18.0686&cnt=10&appid=0857813cedbd03d653fc7a1847fff1ec
+
+// https://api.openweathermap.org/data/2.5/weather?lat=59.3293&lon=18.0686&appid=39cc3cd47c5ea2b190a2f97c31df95bb
